71858f2b3f220a04ed70b4cdb15a9ccc94907d6b
saahil0708 <saahil.ind.dgp@gmail.com>
2026-04-17 14:34:43 +0530

    .

--- Server/src/app.js
+++ Server/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: process.env.FRONTEND_URL || "http://localhost:3000",
+        origin: [process.env.FRONTEND_URL || "http://localhost:3000", "http://localhost:3000"],
         credentials: true
     }
 ));
